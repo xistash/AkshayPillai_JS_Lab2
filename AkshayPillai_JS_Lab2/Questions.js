2ceb54f1dd1f0ca26fd24ca000f55c52b6bba7d3
xistash <103055712+xistash@users.noreply.github.com>
2023-04-04 23:55:14 +0530

    Add files via upload

---
+++ AkshayPillai_JS_Lab2/Questions.js
@@ -0,0 +1,57 @@
+let questions = [
+    {
+    numb: 1,
+    question: "Time travel (traveling to the past or future) has been a popular theme in film and fiction. Albert Einstein offered a groundbreaking theory that gives some scientific possibility to actual time travel. What theory is this?",
+    answer: "Special Relativity Theory",
+    options: [
+      "Big bang Theory",
+      "Special Relativity Theory",
+      "Wave particle duality theory",
+      "Time Travel Theory"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Kevin Bonsor published which book in 2000, relating to time travel?",
+    answer: "How Time travel will work",
+    options: [
+      "Time travel is impossible",
+      "How Time travel will work",
+      "Time travel for Dummies",
+      "Einstein's Time Travel Theory"
+    ]
+  },
+    {
+    numb: 3,
+    question: "There are various space phenomena, in theory, that astronomers could use to travel back and forth in time. Black holes are one option. Which of the following is true about black holes?",
+    answer: "No object can escape their gravitational pull",
+    options: [
+      "The sun is likely to become a black hole",
+      "A black hole's gravity does not bend light",
+      "No object can escape their gravitational pull",
+      "There is no real evidence that black hole exist"
+    ]
+  },
+    {
+    numb: 4,
+    question: " Cosmic strings have been singled out by scientists as possible enablers of time travel. What is the problem with cosmic strings?",
+    answer: "they have not actually been found",
+    options: [
+      "They have no effect on time or space",
+      "they have not actually been found",
+      "They have a two-dimensional singularity.",
+      "All of the above"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Traveling to the past poses more problems than traveling to the future. Many complex obstacles could arise in past time travel, leading physicists to believe that it is impossible. One of the major objections to the possibility of time travel is the Grandfather Paradox What does this paradox state?",
+    answer: "If you went back in time and killed your grandfather, then you could not exist, so you could not have killed your grandfather, etc.",
+    options: [
+      "If you went back in time, you might meet your grandfather and learn secrets about your past.",
+      "If you went back in time and killed your grandfather, then you could not exist, so you could not have killed your grandfather, etc.",
+      "There is no Grandfather Paradox",
+      "If you traveled to the past, you might run into yourself, but how can there be two of you?"
+    ]
+  },
+];
